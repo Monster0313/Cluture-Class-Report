976c1ea8771a081587281db86bcaba16f485c958
Little Monster <monster860313@gmail.com>
2018-10-23 02:57:14 +0800

    Bug fixed

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -10,126 +10,210 @@
       <div class="form-group">
         <span>*Q1: 你的生理性別為？{{ ' ' + newVote.sex }}</span>
         <br>
-        <input type="radio" class="voteSex" name="Sex" value="Male" v-model="newVote.sex" required>
-        <label for="Male">男性</label>
-        <input type="radio" class="voteSex" name="Sex" value="Female" v-model="newVote.sex">
-        <label for="Female">女性</label>
-        <input type="radio" class="voteSex" name="Sex" value="Others" v-model="newVote.sex">
-        <label for="Others">其他</label>
+        <label for="Male">
+          <input type="radio" class="voteSex" name="Sex" value="Male" v-model="newVote.sex" required>
+        男性
+        </label>
+        <label for="Female">
+          <input type="radio" class="voteSex" name="Sex" value="Female" v-model="newVote.sex">
+        女性
+        </label>
+        <label for="Others">
+          <input type="radio" class="voteSex" name="Sex" value="Others" v-model="newVote.sex">
+        其他
+        </label>
       </div>
       <div class="form-group">
         <span>*Q2: 你的性傾向為？{{ ' ' + newVote.orientation }}</span>
         <br>
-        <input type="radio" class="voteOrientation" name="Orientation" value="Homosexual" v-model="newVote.orientation" required>
-        <label for="Homosexual">同性戀</label>
-        <input type="radio" class="voteOrientation" name="Orientation" value="Bisexual" v-model="newVote.orientation">
-        <label for="Bisexual">雙性戀</label>
-        <input type="radio" class="voteOrientation" name="Orientation" value="Heterosexual" v-model="newVote.orientation">
-        <label for="Heterosexual">異性戀</label>
-        <input type="radio" class="voteOrientation" name="Orientation" value="Asexual" v-model="newVote.orientation">
-        <label for="Asexual">無性戀</label>
-        <input type="radio" class="voteOrientation" name="Orientation" value="Pansexual" v-model="newVote.orientation">
-        <label for="Pansexual">泛性戀</label>
-        <input type="radio" class="voteOrientation" name="Orientation" value="Others" v-model="newVote.orientation">
-        <label for="Others">其他</label>
+        <label for="Homosexual">
+          <input type="radio" class="voteOrientation" name="Orientation" value="Homosexual" v-model="newVote.orientation" required>
+        同性戀
+        </label>
+        <label for="Bisexual">
+          <input type="radio" class="voteOrientation" name="Orientation" value="Bisexual" v-model="newVote.orientation">
+        雙性戀
+        </label>
+        <label for="Heterosexual">
+          <input type="radio" class="voteOrientation" name="Orientation" value="Heterosexual" v-model="newVote.orientation">
+        異性戀
+        </label>
+        <label for="Asexual">
+          <input type="radio" class="voteOrientation" name="Orientation" value="Asexual" v-model="newVote.orientation">
+        無性戀
+        </label>
+        <label for="Pansexual">
+          <input type="radio" class="voteOrientation" name="Orientation" value="Pansexual" v-model="newVote.orientation">
+        泛性戀
+        </label>
+        <label for="Others">
+          <input type="radio" class="voteOrientation" name="Orientation" value="Others" v-model="newVote.orientation">
+        其他
+        </label>
       </div>
       <div class="form-group">
         <span>*Q3: 你認為自己的身高？{{ ' ' + newVote.height }}</span>
         <br>
-        <input type="radio" class="voteHeight" name="Height" value="Short" v-model="newVote.height" required>
-        <label for="Short">矮</label>
-        <input type="radio" class="voteHeight" name="Height" value="High" v-model="newVote.height">
-        <label for="High">高</label>
+        <label for="Short">
+          <input type="radio" class="voteHeight" name="Height" value="Short" v-model="newVote.height" required>
+        矮
+        </label>
+        <label for="High">
+          <input type="radio" class="voteHeight" name="Height" value="High" v-model="newVote.height">
+        高
+        </label>
       </div>
       <div class="form-group">
         <span>*Q4: 你認為自己的體重？{{ ' ' + newVote.weight }}</span>
         <br>
-        <input type="radio" class="voteWeight" name="Weight" value="Light" v-model="newVote.weight" required>
-        <label for="Light">輕</label>
-        <input type="radio" class="voteWeight" name="Weight" value="Heavy" v-model="newVote.weight">
-        <label for="Heavy">重</label>
+        <label for="Light">
+          <input type="radio" class="voteWeight" name="Weight" value="Light" v-model="newVote.weight" required>
+        輕
+        </label>
+        <label for="Heavy">
+          <input type="radio" class="voteWeight" name="Weight" value="Heavy" v-model="newVote.weight">
+        重
+        </label>
       </div>
       <div class="form-group">
         <span>*Q5: 你認為自己的經濟狀況？{{ ' ' + newVote.rich }}</span>
         <br>
-        <input type="radio" class="voteRich" name="Rich" value="1" v-model="newVote.rich" required>
-        <label for="1">極低</label>
-        <input type="radio" class="voteRich" name="Rich" value="2" v-model="newVote.rich">
-        <label for="2">低</label>
-        <input type="radio" class="voteRich" name="Rich" value="3" v-model="newVote.rich">
-        <label for="3">中</label>
-        <input type="radio" class="voteRich" name="Rich" value="4" v-model="newVote.rich">
-        <label for="4">高</label>
-        <input type="radio" class="voteRich" name="Rich" value="5" v-model="newVote.rich">
-        <label for="5">極高</label>
+        <label for="very-low">
+          <input type="radio" class="voteRich" name="Rich" value="very-low" v-model="newVote.rich" required>
+        極低
+        </label>
+        <label for="low">
+          <input type="radio" class="voteRich" name="Rich" value="low" v-model="newVote.rich">
+        低
+        </label>
+        <label for="ordinary">
+          <input type="radio" class="voteRich" name="Rich" value="ordinary" v-model="newVote.rich">
+        中
+        </label>
+        <label for="high">
+          <input type="radio" class="voteRich" name="Rich" value="high" v-model="newVote.rich">
+        高
+        </label>
+        <label for="very-high">
+          <input type="radio" class="voteRich" name="Rich" value="very-high" v-model="newVote.rich">
+        極高
+        </label>
       </div>
       <div class="form-group">
         <span>*Q6: 你的學貸狀況？{{ ' ' + newVote.loan }}</span>
         <br>
-        <input type="radio" class="voteLoan" name="Loan" value="True" v-model="newVote.loan" required>
-        <label for="True">有</label>
-        <input type="radio" class="voteLoan" name="Loan" value="False" v-model="newVote.loan">
-        <label for="False">無</label>
+        <label for="True">
+          <input type="radio" class="voteLoan" name="Loan" value="True" v-model="newVote.loan" required>
+        有
+        </label>
+        <label for="False">
+          <input type="radio" class="voteLoan" name="Loan" value="False" v-model="newVote.loan">
+        無
+        </label>
       </div>
       <div class="form-group">
         <span>*Q7: 你的關係狀態？{{ ' ' + newVote.relationship }}</span>
         <br>
-        <input type="radio" class="voteRelationship" name="Relationship" value="0" v-model="newVote.relationship" required>
-        <label for="0">單身</label>
-        <input type="radio" class="voteRelationship" name="Relationship" value="1" v-model="newVote.relationship">
-        <label for="1">非單身</label>
+        <label for="Single">
+          <input type="radio" class="voteRelationship" name="Relationship" value="Single" v-model="newVote.relationship" required>
+        單身
+        </label>
+        <label for="Not-single">
+          <input type="radio" class="voteRelationship" name="Relationship" value="Not-single" v-model="newVote.relationship">
+        非單身
+        </label>
       </div>
       <div class="form-group">
         <span>*Q8: 你的家庭模式？{{ ' ' + newVote.family }}</span>
         <br>
-        <input type="radio" class="voteFamily" name="Family" value="0" v-model="newVote.family" required>
-        <label for="0">單親家庭</label>
-        <input type="radio" class="voteFamily" name="Family" value="1" v-model="newVote.family">
-        <label for="1">雙親家庭</label>
-        <input type="radio" class="voteFamily" name="Family" value="2" v-model="newVote.family">
-        <label for="2">繼親家庭</label>
-        <input type="radio" class="voteFamily" name="Family" value="3" v-model="newVote.family">
-        <label for="3">三代家庭</label>
-        <input type="radio" class="voteFamily" name="Family" value="4" v-model="newVote.family">
-        <label for="4">隔代家庭</label>
-        <input type="radio" class="voteFamily" name="Family" value="5" v-model="newVote.family">
-        <label for="5">其他</label>
+        <label for="One-parent">
+          <input type="radio" class="voteFamily" name="Family" value="One-parent" v-model="newVote.family" required>
+        單親家庭
+        </label>
+        <label for="Two-parent">
+          <input type="radio" class="voteFamily" name="Family" value="Two-parent" v-model="newVote.family">
+        雙親家庭
+        </label>
+        <label for="Stepfamily">
+          <input type="radio" class="voteFamily" name="Family" value="Stepfamily" v-model="newVote.family">
+        繼親家庭
+        </label>
+        <label for="Three-generations">
+          <input type="radio" class="voteFamily" name="Family" value="Three-generations" v-model="newVote.family">
+        三代家庭
+        </label>
+        <label for="Intergenerational">
+          <input type="radio" class="voteFamily" name="Family" value="Intergenerational" v-model="newVote.family">
+        隔代家庭
+        </label>
+        <label for="Others">
+          <input type="radio" class="voteFamily" name="Family" value="Others" v-model="newVote.family">
+        其他
+        </label>
       </div>
       <div class="form-group">
         <span>*Q9: 你的族群認同？{{ ' ' + newVote.population }}</span>
         <br>
-        <input type="radio" class="votePopulation" name="Population" value="Hakka" v-model="newVote.population" required>
-        <label for="Hakka">客家族群</label>
-        <input type="radio" class="votePopulation" name="Population" value="Minnan" v-model="newVote.population">
-        <label for="Minnan">閩南族群</label>
-        <input type="radio" class="votePopulation" name="Population" value="Han" v-model="newVote.population">
-        <label for="Han">漢人</label>
-        <input type="radio" class="votePopulation" name="Population" value="Aborigines" v-model="newVote.population">
-        <label for="Aborigines">原住民族</label>
-        <input type="radio" class="votePopulation" name="Population" value="Immigrant" v-model="newVote.population">
-        <label for="Immigrant">新住民</label>
-        <input type="radio" class="votePopulation" name="Population" value="Others" v-model="newVote.population">
-        <label for="Others">其他</label>
+        <label for="Hakka">
+          <input type="radio" class="votePopulation" name="Population" value="Hakka" v-model="newVote.population" required>
+        客家族群
+        </label>
+        <label for="Minnan">
+          <input type="radio" class="votePopulation" name="Population" value="Minnan" v-model="newVote.population">
+        閩南族群
+        </label>
+        <label for="Han">
+          <input type="radio" class="votePopulation" name="Population" value="Han" v-model="newVote.population">
+        漢人
+        </label>
+        <label for="Aborigines">
+          <input type="radio" class="votePopulation" name="Population" value="Aborigines" v-model="newVote.population">
+        原住民族
+        </label>
+        <label for="Immigrant">
+          <input type="radio" class="votePopulation" name="Population" value="Immigrant" v-model="newVote.population">
+        新住民
+        </label>
+        <label for="Others">
+          <input type="radio" class="votePopulation" name="Population" value="Others" v-model="newVote.population">
+        其他
+        </label>
       </div>
       <div class="form-group">
         <span>*Q10: 你的國籍？{{ ' ' + newVote.country }}</span>
         <br>
-        <input type="radio" class="voteCountry" name="Country" value="PRC" v-model="newVote.country" required>
-        <label for="PRC">中華人民共和國</label>
-        <input type="radio" class="voteCountry" name="Country" value="ROC" v-model="newVote.country">
-        <label for="ROC">中華民國</label>
-        <input type="radio" class="voteCountry" name="Country" value="TWN" v-model="newVote.country">
-        <label for="TWN">台灣</label>
-        <input type="radio" class="voteCountry" name="Country" value="HK" v-model="newVote.country">
-        <label for="HK">香港</label>
-        <input type="radio" class="voteCountry" name="Country" value="MO" v-model="newVote.country">
-        <label for="MO">澳門</label>
-        <input type="radio" class="voteCountry" name="Country" value="MYS" v-model="newVote.country">
-        <label for="MYS">馬來西亞</label>
-        <input type="radio" class="voteCountry" name="Country" value="KOR" v-model="newVote.country">
-        <label for="KOR">大韓民國</label>
-        <input type="radio" class="voteCountry" name="Country" value="Others" v-model="newVote.country">
-        <label for="Others">其他</label>
+        <label for="PRC">
+          <input type="radio" class="voteCountry" name="Country" value="PRC" v-model="newVote.country" required>
+        中華人民共和國
+        </label>
+        <label for="ROC">
+          <input type="radio" class="voteCountry" name="Country" value="ROC" v-model="newVote.country">
+        中華民國
+        </label>
+        <label for="TWN">
+          <input type="radio" class="voteCountry" name="Country" value="TWN" v-model="newVote.country">
+        台灣
+        </label>
+        <label for="HK">
+          <input type="radio" class="voteCountry" name="Country" value="HK" v-model="newVote.country">
+        香港
+        </label>
+        <label for="MO">
+          <input type="radio" class="voteCountry" name="Country" value="MO" v-model="newVote.country">
+        澳門
+        </label>
+        <label for="MYS">
+          <input type="radio" class="voteCountry" name="Country" value="MYS" v-model="newVote.country">
+        馬來西亞
+        </label>
+        <label for="KOR">
+          <input type="radio" class="voteCountry" name="Country" value="KOR" v-model="newVote.country">
+        大韓民國
+        </label>
+        <label for="Others">
+          <input type="radio" class="voteCountry" name="Country" value="Others" v-model="newVote.country">
+        其他
+        </label>
       </div>
       <input type="submit" class="submit-button" value="送出">
     </form>
